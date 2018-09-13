996b62aa977f6ce487bb7b9cbbce898e69f2cdfa
dengzhongwen <dengzhongwen@cmschina.com.cn>
2018-09-13 15:23:21 +0800

    add cluster SUITE case

---
+++ test/eetcd_cluster_SUITE.erl
@@ -0,0 +1,63 @@
+
+-module(eetcd_cluster_SUITE).
+
+%% API
+
+-export([all/0, suite/0, groups/0, init_per_suite/1, end_per_suite/1]).
+
+-export([member/1]).
+
+-include("router_pb.hrl").
+
+suite() ->
+    [{timetrap, {minutes, 1}}].
+
+all() ->
+    [
+        member
+    ].
+
+groups() ->
+    [].
+
+init_per_suite(Config) ->
+    application:ensure_all_started(eetcd),
+    Config.
+
+end_per_suite(_Config) ->
+    application:stop(eetcd),
+    ok.
+
+member(_Config) ->
+    %% TODO HOW TO
+    %% add
+    %% peerURLs is the list of URLs the added member will use to communicate with the cluster.
+    %% member is the member information for the added member.
+    %% members is a list of all members after adding the new member.
+    %% FakePeerURLs = [<<"http://127.0.0.1:2480">>],
+    %% {ok,#'Etcd.MemberAddResponse'{
+    %%    header =#'Etcd.ResponseHeader'{},
+    %%    member = #'Etcd.Member'{peerURLs = FakePeerURLs, clientURLs = []},
+    %%    members = [_, _]}}
+    %%    = eetcd_cluster:member_add(#'Etcd.MemberAddRequest'{'peerURLs' = FakePeerURLs}),
+    
+    %% list
+    %% members is a list of all members associated with the cluster.
+    {ok,#'Etcd.MemberListResponse'{members = [_]}}
+        = eetcd_cluster:member_list(#'Etcd.MemberListRequest'{}),
+    %% #'Etcd.Member'{'ID' = ID} = lists:keyfind(FakePeerURLs, #'Etcd.Member'.peerURLs, Members),
+    
+    %% update
+    %% ID is the member ID of the member to update.
+    %% peerURLs is the new list of URLs the member will use to communicate with the cluster.
+    %% members is a list of all members after updating the member.
+    %% TODO NOT WORK
+    %% {ok, #'Etcd.MemberUpdateResponse'{members = Members2}}
+    %%    = eetcd_cluster:member_update(#'Etcd.MemberUpdateRequest'{'ID' = ID, peerURLs = [<<"http://127.0.0.1:2480">>]}),
+    %% #'Etcd.Member'{'ID' = ID} = lists:keyfind([<<"http://127.0.0.1:2480">>], #'Etcd.Member'.peerURLs, Members2),
+    
+    %% remove
+    %% members is a list of all members after updating the member.
+    %% {ok, #'Etcd.MemberRemoveResponse'{members = [_]}}
+    %%   = eetcd_cluster:member_remove(#'Etcd.MemberRemoveRequest'{'ID' = ID}),
+    ok.

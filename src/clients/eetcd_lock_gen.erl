74c73e8c670f936631ebc5319786cef26363a9e2
Michael Klishin <michael@clojurewerkz.org>
2020-03-26 20:34:48 +0300

    Regenerate with rebar3 etcd gen

---
+++ src/clients/eetcd_lock_gen.erl
@@ -0,0 +1,26 @@
+%%%-------------------------------------------------------------------
+%% @doc Behaviour to implement for eectd Etcd.Lock.
+%% @private
+%%  All detail documents please visit https://github.com/etcd-io/etcd/blob/master/Documentation/dev-guide/api_reference_v3.md
+%% @end
+%%%-------------------------------------------------------------------
+
+%% This module was generated on 2020-03-26T17:32:58+00:00 and should not be modified manually
+
+-module(eetcd_lock_gen).
+
+-export([lock/1]).
+-export([unlock/1]).
+
+%% @doc Unary RPC 
+-spec lock(router_pb:'Etcd.LockRequest'()) ->
+    {ok, router_pb:'Etcd.LockResponse'()}|{error,eetcd:eetcd_error()}.
+lock(Request) ->
+    eetcd_stream:unary(Request, 'Etcd.LockRequest', <<"/etcdserverpb.Lock/Lock">>, 'Etcd.LockResponse').
+
+%% @doc Unary RPC 
+-spec unlock(router_pb:'Etcd.UnlockRequest'()) ->
+    {ok, router_pb:'Etcd.UnlockResponse'()}|{error,eetcd:eetcd_error()}.
+unlock(Request) ->
+    eetcd_stream:unary(Request, 'Etcd.UnlockRequest', <<"/etcdserverpb.Lock/Unlock">>, 'Etcd.UnlockResponse').
+

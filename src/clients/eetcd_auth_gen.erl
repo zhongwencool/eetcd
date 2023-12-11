12be64dc6a0acd1b5d62b8367731b11e5d19de7b
Gilbert <gilbertwong96@icloud.com>
2023-12-11 19:14:47 +0800

    Update rpc.proto and re-generate protobuf codes
    The proto file appears to be somewhat outdated, lacking the latest
    API definitions for etcd APIs.
    
    In this update, I have upgraded the proto files and removed any stale code.

--- src/clients/eetcd_auth_gen.erl
+++ src/clients/eetcd_auth_gen.erl
@@ -1,16 +1,17 @@
 %%%-------------------------------------------------------------------
-%% @doc Behaviour to implement for eectd Etcd.Auth
+%% @doc Behaviour to implement for eetcd etcdserverpb.Auth
 %% @private
 %%  All detail documents please visit https://github.com/etcd-io/etcd/blob/master/Documentation/dev-guide/api_reference_v3.md
 %% @end
 %%%-------------------------------------------------------------------
 
-%% This module was generated on 2020-04-02T02:00:45+00:00 and should not be modified manually
+%% This module was generated on 2023-12-11T09:36:27+00:00 and should not be modified manually
 
 -module(eetcd_auth_gen).
 
 -export([auth_enable/1]).
 -export([auth_disable/1]).
+-export([auth_status/1]).
 -export([authenticate/1]).
 -export([user_add/1]).
 -export([user_get/1]).
@@ -26,99 +27,105 @@
 -export([role_grant_permission/1]).
 -export([role_revoke_permission/1]).
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/AuthEnable" 
--spec auth_enable(router_pb:'Etcd.AuthEnableRequest'()) ->
-    {ok, router_pb:'Etcd.AuthEnableResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/AuthEnable"
+-spec auth_enable(rpc_pb:'etcdserverpb.AuthEnableRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthEnableResponse'()}|{error,eetcd:eetcd_error()}.
 auth_enable(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthEnableRequest', <<"/etcdserverpb.Auth/AuthEnable">>, 'Etcd.AuthEnableResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthEnableRequest', <<"/etcdserverpb.Auth/AuthEnable">>, 'etcdserverpb.AuthEnableResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/AuthDisable" 
--spec auth_disable(router_pb:'Etcd.AuthDisableRequest'()) ->
-    {ok, router_pb:'Etcd.AuthDisableResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/AuthDisable"
+-spec auth_disable(rpc_pb:'etcdserverpb.AuthDisableRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthDisableResponse'()}|{error,eetcd:eetcd_error()}.
 auth_disable(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthDisableRequest', <<"/etcdserverpb.Auth/AuthDisable">>, 'Etcd.AuthDisableResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthDisableRequest', <<"/etcdserverpb.Auth/AuthDisable">>, 'etcdserverpb.AuthDisableResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/Authenticate" 
--spec authenticate(router_pb:'Etcd.AuthenticateRequest'()) ->
-    {ok, router_pb:'Etcd.AuthenticateResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/AuthStatus"
+-spec auth_status(rpc_pb:'etcdserverpb.AuthStatusRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthStatusResponse'()}|{error,eetcd:eetcd_error()}.
+auth_status(Request) ->
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthStatusRequest', <<"/etcdserverpb.Auth/AuthStatus">>, 'etcdserverpb.AuthStatusResponse').
+
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/Authenticate"
+-spec authenticate(rpc_pb:'etcdserverpb.AuthenticateRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthenticateResponse'()}|{error,eetcd:eetcd_error()}.
 authenticate(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthenticateRequest', <<"/etcdserverpb.Auth/Authenticate">>, 'Etcd.AuthenticateResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthenticateRequest', <<"/etcdserverpb.Auth/Authenticate">>, 'etcdserverpb.AuthenticateResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserAdd" 
--spec user_add(router_pb:'Etcd.AuthUserAddRequest'()) ->
-    {ok, router_pb:'Etcd.AuthUserAddResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserAdd"
+-spec user_add(rpc_pb:'etcdserverpb.AuthUserAddRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthUserAddResponse'()}|{error,eetcd:eetcd_error()}.
 user_add(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthUserAddRequest', <<"/etcdserverpb.Auth/UserAdd">>, 'Etcd.AuthUserAddResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthUserAddRequest', <<"/etcdserverpb.Auth/UserAdd">>, 'etcdserverpb.AuthUserAddResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserGet" 
--spec user_get(router_pb:'Etcd.AuthUserGetRequest'()) ->
-    {ok, router_pb:'Etcd.AuthUserGetResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserGet"
+-spec user_get(rpc_pb:'etcdserverpb.AuthUserGetRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthUserGetResponse'()}|{error,eetcd:eetcd_error()}.
 user_get(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthUserGetRequest', <<"/etcdserverpb.Auth/UserGet">>, 'Etcd.AuthUserGetResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthUserGetRequest', <<"/etcdserverpb.Auth/UserGet">>, 'etcdserverpb.AuthUserGetResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserList" 
--spec user_list(router_pb:'Etcd.AuthUserListRequest'()) ->
-    {ok, router_pb:'Etcd.AuthUserListResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserList"
+-spec user_list(rpc_pb:'etcdserverpb.AuthUserListRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthUserListResponse'()}|{error,eetcd:eetcd_error()}.
 user_list(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthUserListRequest', <<"/etcdserverpb.Auth/UserList">>, 'Etcd.AuthUserListResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthUserListRequest', <<"/etcdserverpb.Auth/UserList">>, 'etcdserverpb.AuthUserListResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserDelete" 
--spec user_delete(router_pb:'Etcd.AuthUserDeleteRequest'()) ->
-    {ok, router_pb:'Etcd.AuthUserDeleteResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserDelete"
+-spec user_delete(rpc_pb:'etcdserverpb.AuthUserDeleteRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthUserDeleteResponse'()}|{error,eetcd:eetcd_error()}.
 user_delete(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthUserDeleteRequest', <<"/etcdserverpb.Auth/UserDelete">>, 'Etcd.AuthUserDeleteResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthUserDeleteRequest', <<"/etcdserverpb.Auth/UserDelete">>, 'etcdserverpb.AuthUserDeleteResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserChangePassword" 
--spec user_change_password(router_pb:'Etcd.AuthUserChangePasswordRequest'()) ->
-    {ok, router_pb:'Etcd.AuthUserChangePasswordResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserChangePassword"
+-spec user_change_password(rpc_pb:'etcdserverpb.AuthUserChangePasswordRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthUserChangePasswordResponse'()}|{error,eetcd:eetcd_error()}.
 user_change_password(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthUserChangePasswordRequest', <<"/etcdserverpb.Auth/UserChangePassword">>, 'Etcd.AuthUserChangePasswordResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthUserChangePasswordRequest', <<"/etcdserverpb.Auth/UserChangePassword">>, 'etcdserverpb.AuthUserChangePasswordResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserGrantRole" 
--spec user_grant_role(router_pb:'Etcd.AuthUserGrantRoleRequest'()) ->
-    {ok, router_pb:'Etcd.AuthUserGrantRoleResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserGrantRole"
+-spec user_grant_role(rpc_pb:'etcdserverpb.AuthUserGrantRoleRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthUserGrantRoleResponse'()}|{error,eetcd:eetcd_error()}.
 user_grant_role(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthUserGrantRoleRequest', <<"/etcdserverpb.Auth/UserGrantRole">>, 'Etcd.AuthUserGrantRoleResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthUserGrantRoleRequest', <<"/etcdserverpb.Auth/UserGrantRole">>, 'etcdserverpb.AuthUserGrantRoleResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserRevokeRole" 
--spec user_revoke_role(router_pb:'Etcd.AuthUserRevokeRoleRequest'()) ->
-    {ok, router_pb:'Etcd.AuthUserRevokeRoleResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/UserRevokeRole"
+-spec user_revoke_role(rpc_pb:'etcdserverpb.AuthUserRevokeRoleRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthUserRevokeRoleResponse'()}|{error,eetcd:eetcd_error()}.
 user_revoke_role(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthUserRevokeRoleRequest', <<"/etcdserverpb.Auth/UserRevokeRole">>, 'Etcd.AuthUserRevokeRoleResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthUserRevokeRoleRequest', <<"/etcdserverpb.Auth/UserRevokeRole">>, 'etcdserverpb.AuthUserRevokeRoleResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleAdd" 
--spec role_add(router_pb:'Etcd.AuthRoleAddRequest'()) ->
-    {ok, router_pb:'Etcd.AuthRoleAddResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleAdd"
+-spec role_add(rpc_pb:'etcdserverpb.AuthRoleAddRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthRoleAddResponse'()}|{error,eetcd:eetcd_error()}.
 role_add(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthRoleAddRequest', <<"/etcdserverpb.Auth/RoleAdd">>, 'Etcd.AuthRoleAddResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthRoleAddRequest', <<"/etcdserverpb.Auth/RoleAdd">>, 'etcdserverpb.AuthRoleAddResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleGet" 
--spec role_get(router_pb:'Etcd.AuthRoleGetRequest'()) ->
-    {ok, router_pb:'Etcd.AuthRoleGetResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleGet"
+-spec role_get(rpc_pb:'etcdserverpb.AuthRoleGetRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthRoleGetResponse'()}|{error,eetcd:eetcd_error()}.
 role_get(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthRoleGetRequest', <<"/etcdserverpb.Auth/RoleGet">>, 'Etcd.AuthRoleGetResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthRoleGetRequest', <<"/etcdserverpb.Auth/RoleGet">>, 'etcdserverpb.AuthRoleGetResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleList" 
--spec role_list(router_pb:'Etcd.AuthRoleListRequest'()) ->
-    {ok, router_pb:'Etcd.AuthRoleListResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleList"
+-spec role_list(rpc_pb:'etcdserverpb.AuthRoleListRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthRoleListResponse'()}|{error,eetcd:eetcd_error()}.
 role_list(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthRoleListRequest', <<"/etcdserverpb.Auth/RoleList">>, 'Etcd.AuthRoleListResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthRoleListRequest', <<"/etcdserverpb.Auth/RoleList">>, 'etcdserverpb.AuthRoleListResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleDelete" 
--spec role_delete(router_pb:'Etcd.AuthRoleDeleteRequest'()) ->
-    {ok, router_pb:'Etcd.AuthRoleDeleteResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleDelete"
+-spec role_delete(rpc_pb:'etcdserverpb.AuthRoleDeleteRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthRoleDeleteResponse'()}|{error,eetcd:eetcd_error()}.
 role_delete(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthRoleDeleteRequest', <<"/etcdserverpb.Auth/RoleDelete">>, 'Etcd.AuthRoleDeleteResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthRoleDeleteRequest', <<"/etcdserverpb.Auth/RoleDelete">>, 'etcdserverpb.AuthRoleDeleteResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleGrantPermission" 
--spec role_grant_permission(router_pb:'Etcd.AuthRoleGrantPermissionRequest'()) ->
-    {ok, router_pb:'Etcd.AuthRoleGrantPermissionResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleGrantPermission"
+-spec role_grant_permission(rpc_pb:'etcdserverpb.AuthRoleGrantPermissionRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthRoleGrantPermissionResponse'()}|{error,eetcd:eetcd_error()}.
 role_grant_permission(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthRoleGrantPermissionRequest', <<"/etcdserverpb.Auth/RoleGrantPermission">>, 'Etcd.AuthRoleGrantPermissionResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthRoleGrantPermissionRequest', <<"/etcdserverpb.Auth/RoleGrantPermission">>, 'etcdserverpb.AuthRoleGrantPermissionResponse').
 
-%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleRevokePermission" 
--spec role_revoke_permission(router_pb:'Etcd.AuthRoleRevokePermissionRequest'()) ->
-    {ok, router_pb:'Etcd.AuthRoleRevokePermissionResponse'()}|{error,eetcd:eetcd_error()}.
+%% @doc Unary RPC for service at path "/etcdserverpb.Auth/RoleRevokePermission"
+-spec role_revoke_permission(rpc_pb:'etcdserverpb.AuthRoleRevokePermissionRequest'()) ->
+    {ok, rpc_pb:'etcdserverpb.AuthRoleRevokePermissionResponse'()}|{error,eetcd:eetcd_error()}.
 role_revoke_permission(Request) ->
-    eetcd_stream:unary(Request, 'Etcd.AuthRoleRevokePermissionRequest', <<"/etcdserverpb.Auth/RoleRevokePermission">>, 'Etcd.AuthRoleRevokePermissionResponse').
+    eetcd_stream:unary(Request, 'etcdserverpb.AuthRoleRevokePermissionRequest', <<"/etcdserverpb.Auth/RoleRevokePermission">>, 'etcdserverpb.AuthRoleRevokePermissionResponse').
 
--- src/eetcd_maintenance.erl
+++ src/eetcd_maintenance.erl
@@ -6,7 +6,7 @@
 
 %%% @doc AlarmList gets all active alarms.
 -spec alarm_list(new_context()) ->
-    {ok,router_pb:'Etcd.AlarmResponse'()}|{error,eetcd_error()}.
+    {ok,rpc_pb:'etcdserverpb.AlarmResponse'()}|{error,eetcd_error()}.
 alarm_list(ConnName) ->
     C1 = eetcd:new(ConnName),
     C2 = maps:put(action, 'GET', C1),
@@ -16,7 +16,7 @@ alarm_list(ConnName) ->
 
 %%% @doc AlarmDisarm disarms a given alarm.
 -spec alarm_disarm(new_context(), integer(), integer()) ->
-    {ok,router_pb:'Etcd.AlarmResponse'()}|{error,eetcd_error()}.
+    {ok,rpc_pb:'etcdserverpb.AlarmResponse'()}|{error,eetcd_error()}.
 alarm_disarm(Context, MemberId, Alarm) ->
     C1 = eetcd:new(Context),
     C2 = maps:put(action, 'DEACTIVATE', C1),
@@ -26,7 +26,7 @@ alarm_disarm(Context, MemberId, Alarm) ->
 
 %%% @doc AlarmDisarmAll disarms all alarm.
 -spec alarm_disarm_all(new_context()) ->
-    router_pb:'Etcd.AlarmResponse'().
+    rpc_pb:'etcdserverpb.AlarmResponse'().
 alarm_disarm_all(ConnName) ->
     {ok, Acc0 = #{alarms := List}} = alarm_list(ConnName),
     lists:foldl(
@@ -47,14 +47,14 @@ alarm_disarm_all(ConnName) ->
 %%% To defragment multiple members in the cluster, user need to call defragment multiple
 %%% times with different endpoints.
 -spec defragment(iodata(), eetcd:opts()) ->
-    {ok,router_pb:'Etcd.DefragmentResponse'()}|{error,eetcd_error()}.
+    {ok,rpc_pb:'etcdserverpb.DefragmentResponse'()}|{error,eetcd_error()}.
 defragment(Endpoint, Options) ->
     Fun = fun(Conn) -> eetcd_maintenance_gen:defragment(eetcd:new(Conn)) end,
     dial(Endpoint, Options, Fun).
 
 %%% @doc Status gets the status of the endpoint.
 -spec status(iodata(), eetcd:opts()) ->
-    {ok,router_pb:'Etcd.StatusResponse'()}|{error,eetcd_error()}.
+    {ok,rpc_pb:'etcdserverpb.StatusResponse'()}|{error,eetcd_error()}.
 status(Endpoint, Options) ->
     Fun = fun(Conn) -> eetcd_maintenance_gen:status(eetcd:new(Conn)) end,
     dial(Endpoint, Options, Fun).
@@ -63,7 +63,7 @@ status(Endpoint, Options) ->
 %%% If revision is zero, the hash is computed on all keys. If the revision
 %%% is non-zero, the hash is computed on all keys at or below the given revision.
 -spec hash_kv(iodata(), eetcd:opts(), pos_integer()) ->
-    {ok,router_pb:'Etcd.HashKVResponse'()}|{error,eetcd_error()}.
+    {ok,rpc_pb:'etcdserverpb.HashKVResponse'()}|{error,eetcd_error()}.
 hash_kv(Endpoint, Options, Rev) ->
     Fun = fun(Conn) ->
         Context = maps:put(revision, Rev, eetcd:new(Conn)),
@@ -80,7 +80,7 @@ hash_kv(Endpoint, Options, Rev) ->
 %%% @doc MoveLeader requests current leader to transfer its leadership to the transferee.
 %%% Request must be made to the leader.
 -spec move_leader(new_context(), pos_integer()) ->
-    {ok,router_pb:'Etcd.MoveLeaderResponse'()}|{error,eetcd_error()}.
+    {ok,rpc_pb:'etcdserverpb.MoveLeaderResponse'()}|{error,eetcd_error()}.
 move_leader(Context, TargetID) ->
     C1 = eetcd:new(Context),
     C2 = maps:put(targetID, TargetID, C1),

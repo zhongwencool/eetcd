e50f38783ce082943964fa70fdc3ddc985f4f7e9
belltoy <belltoy@gmail.com>
2025-07-06 12:18:04 +0800

    Enable auth_SUITE
    Test username/password case instead of no_password, which is used for TLS CN auth only.

--- test/eetcd_auth_SUITE.erl
+++ test/eetcd_auth_SUITE.erl
@@ -5,6 +5,9 @@
 -export([auth/1, user/1]).
 
 -define(Name, ?MODULE).
+-define(ENDPOINTS, ["127.0.0.1:2379", "127.0.0.1:2479", "127.0.0.1:2579"]).
+-define(USERNAME, <<"root">>).
+-define(PASSWORD, <<"password">>).
 
 %% First create root user with root role
 %% ETCDCTL_API=3 etcdctl user add root
@@ -14,17 +17,17 @@ suite() ->
     [{timetrap, {minutes, 3}}].
 
 %% todo
-%% all() -> [auth, user].
-all() -> [].
+all() -> [auth, user].
 
 groups() ->
     [].
 
 init_per_suite(Config) ->
     application:ensure_all_started(eetcd),
-    {ok, _Pid} = eetcd:open(?Name, ["127.0.0.1:2379", "127.0.0.1:2479", "127.0.0.1:2579"]),
-    eetcd_auth_gen:user_add(?Name, #{name => "root", options => #{no_password => true}}),
+    {ok, _Pid} = eetcd:open(?Name, ?ENDPOINTS),
+    eetcd_auth_gen:user_add(?Name, #{name => ?USERNAME, password => ?PASSWORD}),
     eetcd_auth_gen:user_grant_role(?Name, #{user => "root", role => "root"}),
+    eetcd:close(?Name),
     Config.
 
 end_per_suite(_Config) ->
@@ -33,12 +36,23 @@ end_per_suite(_Config) ->
     ok.
 
 auth(_Config) ->
+    {ok, _} = eetcd:open(?Name, ?ENDPOINTS),
     {ok, #{}} = eetcd_auth_gen:auth_enable(?Name, #{}),
+    eetcd:close(?Name),
+
+    {ok, _} = eetcd:open(?Name, ?ENDPOINTS, [{name, ?USERNAME}, {password, ?PASSWORD}]),
     {ok, #{}} = eetcd_auth_gen:auth_disable(?Name, #{}),
+    eetcd:close(?Name),
     ok.
 
-user(EtcdName) ->
+user(_Config) ->
+    EtcdName = user_client, %% The etcd client name cannot be `user' because there is already
+                            %% a system process registered with `user' name.
+    {ok, _} = eetcd:open(EtcdName, ?ENDPOINTS),
     {ok, #{}} = eetcd_auth_gen:auth_enable(EtcdName, #{}),
+    eetcd:close(EtcdName),
+
+    {ok, _} = eetcd:open(EtcdName, ?ENDPOINTS, [{name, ?USERNAME}, {password, ?PASSWORD}]),
     %% list
     {ok, _AuthRoleListResponse} = eetcd_auth_gen:user_list(EtcdName, #{}),
     %% add
@@ -57,5 +71,5 @@ user(EtcdName) ->
     %% list
     {ok, _AuthRoleListResponse2} = eetcd_auth_gen:user_list(EtcdName, #{}),
     {ok, _AuthDisableResponse2} = eetcd_auth_gen:auth_disable(EtcdName, #{}),
+    eetcd:close(EtcdName),
     ok.
-
